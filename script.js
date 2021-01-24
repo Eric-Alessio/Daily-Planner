b761891f136c3f0de1e8fac3b3a7bde64cf9d68e
Eric-Alessio <74992578+Eric-Alessio@users.noreply.github.com>
2021-01-23 22:11:55 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+
+var currentDate = $("#currentDay");
+
+var months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+var days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
